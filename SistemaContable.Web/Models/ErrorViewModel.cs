f836928656945a5fc7bd8f0fa4ae520448e3d1dc
Pedro <Pedro@DESKTOP-OEOSE8L>
2026-01-26 12:12:23 -0600

    Agregar archivos de proyecto.

---
+++ SistemaContable.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaContable.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
